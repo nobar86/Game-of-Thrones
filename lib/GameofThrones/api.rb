f7ecd81fd9907341375addc0eb93b724367da9de
Michael Rabon <nobar86@DESKTOP-89P6Q6H.localdomain>
2020-08-21 16:47:43 -0400

    finished API class

--- lib/GameofThrones/api.rb
+++ lib/GameofThrones/api.rb
@@ -1,12 +1,16 @@
 class API
-
-
-    def self.get_GoT(houses)
-
-        url = "https://www.anapioficeandfire.com/api/houses"
+   
+    def self.get_characters(input)
+        url = "https://www.anapioficeandfire.com/api/houses/#{input}"
         response = HTTParty.get(url)
+        response.each do |character| 
+            name = character["name"]
+            culture = character["culture"]
+            aliases = character["aliases"]
+            allegiances = character["allegiances"]
+            Characters.new(name, culture, aliases, allegiances)
+            Characters
         
+        end
     end
-
-
 end
